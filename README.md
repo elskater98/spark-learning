5fdee9046325c976157f8af946839ec66f6ad9b8
elskater98 <elskater98@gmail.com>
2021-06-09 22:13:47 +0200

    Standalone Mandatory

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # SparkActivity
+
+## Docker
+### Jupyter Notebook
+    docker pull jupyter/all-spark-notebook
+
+    docker run -it --rm -p 8888:8888 -v /home/francesc/work:/home/jovyan/work -w /home/jovyan/work jupyter/all-spark-notebook
+### Spark
+        docker pull sequenceiq/spark:1.6.0
+
+        docker run -it -p 8088:8088 -p 8042:8042 -h sandbox sequenceiq/spark:1.6.0 bash
--- main.py
+++ main.py
@@ -1,8 +1,33 @@
+import json
 import time
-
+import shutil
 import pyspark
 
 if __name__ == '__main__':
     start_time = time.time()
     # https://guru99.es/pyspark-tutorial/
-    sc = pyspark.SparkContext()
+    # Create Spark Context
+    sc = pyspark.SparkContext('local[*]', appName='SparkActivity')
+
+    # Read Input Files
+    # inputFiles = sc.textFile("hdfs:///shared/nando/data/tweets/tweets*.json")
+    inputFiles = sc.textFile('input/tweets/tweets.json')
+    shutil.rmtree('out/', ignore_errors=True)
+
+    # Typescript: arr.map((item) => item+1)
+
+    lower_data = inputFiles.map(lambda i: json.loads(i.lower()))
+
+    # Clean Fields
+    clean_data = lower_data.filter(lambda t: t["entities"]["hashtags"] != []).map(
+        lambda t: {"text": t["text"], "lang": t["lang"],
+                   "hashtags": [x['text'] for x in t["entities"]["hashtags"]]}).filter(lambda i: "es" in i["lang"])
+
+    # Trending Topics
+    trending_data = clean_data.flatMap(lambda h: h['hashtags']).map(lambda hashtag: (hashtag, 1)).reduceByKey(
+        lambda a, b: a + b)
+
+    TopN = sc.parallelize(trending_data.takeOrdered(10, lambda t: -t[1]))
+    TopN.saveAsTextFile("out")
+
+
