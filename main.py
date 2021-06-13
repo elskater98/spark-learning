44ea35476929e87f6232f1174dda22a5c5358c2a
elskater98 <elskater98@gmail.com>
2021-06-13 17:18:01 +0200

    -

--- main.py
+++ main.py
@@ -21,42 +21,38 @@ def sentiments(tweet):
 
     return [(hashtag, (ratio / float(len(tweet['text'])))) for hashtag in tweet['hashtags']]
 
+start_time = time.time()
+# https://guru99.es/pyspark-tutorial/
+# Create Spark Context
+sc = pyspark.SparkContext('local[*]', appName='SparkActivity')
+#os.system("hdfs dfs -rm -R /user/fcp5/SparkActivity")
 
-if __name__ == '__main__':
-    start_time = time.time()
-    # https://guru99.es/pyspark-tutorial/
-    # Create Spark Context
-    os.system("hdfs dfs -rm -R /user/fcp5/SparkActivity")
-    sc = pyspark.SparkContext('local[*]', appName='SparkActivity')
+# Read Input Files
+#inputFiles = sc.textFile("hdfs:///shared/nando/data/tweets/tweets*.json")
+inputFiles = sc.textFile('input/tweets/tweets.json')
+shutil.rmtree('out/', ignore_errors=True)
 
-    # Read Input Files
-    inputFiles = sc.textFile("hdfs:///shared/nando/data/tweets/tweets*.json")
-    # inputFiles = sc.textFile('input/tweets/tweets.json')
-    # shutil.rmtree('out/', ignore_errors=True)
+# Typescript: arr.map((item) => item+1)
+lower_data = inputFiles.map(lambda i: json.loads(i.lower()))
 
-    # Typescript: arr.map((item) => item+1)
+# Clean Fields
+clean_data = lower_data.filter(lambda i: i["entities"]["hashtags"] != []).map(
+    lambda i: {"text": i["text"], "lang": i["lang"],
+               "hashtags": [j['text'] for j in i["entities"]["hashtags"]]}).filter(lambda i: "es" in i["lang"])
 
-    lower_data = inputFiles.map(lambda i: json.loads(i.lower()))
+# Trending Topics
+# https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.RDD.flatMap.html
+trending_data = clean_data.flatMap(lambda i: i['hashtags']).map(lambda hashtag: (hashtag, 1)).reduceByKey(
+    lambda x, y: x + y)
 
-    # Clean Fields
-    clean_data = lower_data.filter(lambda i: i["entities"]["hashtags"] != []).map(
-        lambda i: {"text": i["text"], "lang": i["lang"],
-                   "hashtags": [j['text'] for j in i["entities"]["hashtags"]]}).filter(lambda i: "es" in i["lang"])
+topn = sc.parallelize(trending_data.takeOrdered(10, lambda i: -i[1]))
+topn.saveAsTextFile("out")
+#topn.saveAsTextFile("hdfs:///user/fcp5/SparkActivity/out")
 
-    # Trending Topics
-    # https://spark.apache.org/docs/3.1.1/api/python/reference/api/pyspark.RDD.flatMap.html
-    trending_data = clean_data.flatMap(lambda i: i['hashtags']).map(lambda hashtag: (hashtag, 1)).reduceByKey(
-        lambda x, y: x + y)
+positive_words = set(line.strip().lower() for line in open("input/data/positive_words_es.txt"))
+negative_words = set(line.strip().lower() for line in open("input/data/negative_words_es.txt"))
 
-    topn = sc.parallelize(trending_data.takeOrdered(10, lambda i: -i[1]))
-    #topn.saveAsTextFile("out")
-    topn.saveAsTextFile("hdfs:///user/fcp5/SparkActivity/out")
+_sentiments = clean_data.flatMap(lambda i: sentiments(i)).collect()
 
-    positive_words = set(line.strip().lower() for line in open("input/data/positive_words_es.txt"))
-    negative_words = set(line.strip().lower() for line in open("input/data/negative_words_es.txt"))
-
-    sentiments = clean_data.flatMap(lambda i: sentiments(i)).collect()
-
-    print(sc.parallelize(sentiments).reduceByKey(lambda x, y: x + y).collect())
-
-    print((time.time() - start_time) * 1000)
+print(sc.parallelize(_sentiments).reduceByKey(lambda x, y: x + y).collect())
+print((time.time() - start_time) * 1000)
